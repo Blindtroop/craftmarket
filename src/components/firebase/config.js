c40640afd7914c9367967c9079b09624a322b088
Blindtroop <kaluhilance4@gmail.com>
2024-10-29 10:40:30 +0300

    Fix: Added the env file

--- src/components/firebase/config.js
+++ src/components/firebase/config.js
@@ -3,19 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from 'firebase/auth';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBRWhaqxtdcUf2jMFyCsA_XUjgMfgW8cVc",
-  authDomain: "zenos-33415.firebaseapp.com",
-  projectId: "zenos-33415",
-  storageBucket: "zenos-33415.appspot.com",
-  messagingSenderId: "155862416923",
-  appId: "1:155862416923:web:12a43250c0c12125b402be",
-  measurementId: "G-CSSTX7KV8Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
